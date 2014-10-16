b54fc9bf13399aab623024c80bc8801316419714
Thomas Hutchinson <tomcs251@gmail.com>
2014-10-16 21:32:14 +0200

    imported mongodb client

--- survey-ws.js
+++ survey-ws.js
@@ -14,7 +14,9 @@ var mongodb = require('mongodb');
 var uri = 'mongodb://tawesome:#test3nv#@ds047040.mongolab.com:47040/survey';
 
 mongodb.MongoClient.connect(uri, function(err, db) {
-	//Maybe bind the db to the app context?
+	//Maybe bind the db to the app context? Whilst connecting 
+	//place a InMemoryDB, when connected persist contents of InMemoryDB
+	//to DB and switch to Mongo 
 	app.set("db", db)
 })
 
