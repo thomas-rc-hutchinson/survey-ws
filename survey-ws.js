7d2a02769dddb45065d6ba16705294245aeaa026
Thomas Hutchinson <tomcs251@gmail.com>
2014-10-16 21:33:50 +0200

    removed db connection string, duh!

--- survey-ws.js
+++ survey-ws.js
@@ -11,7 +11,6 @@ app.listen(1222);
 console.log("Listening on port 1222...");
 
 var mongodb = require('mongodb');
-var uri = 'mongodb://tawesome:#test3nv#@ds047040.mongolab.com:47040/survey';
 
 mongodb.MongoClient.connect(uri, function(err, db) {
 	//Maybe bind the db to the app context? Whilst connecting 
